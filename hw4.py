0af9c4c7153133f77515c22bdd238e0d42570ed7
anseonghyeon123 <113087592+anseonghyeon123@users.noreply.github.com>
2024-04-11 22:46:06 +0900

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -1,14 +1,13 @@
-def get_and_greet_name():
-
+def greet():
     name = input("Input his/her name: ")
 
     msg1 = f"Hello {name},"
     msg2 = "Welcome to Seoul."
 
-    max_length = max(len(msg1), len(msg2))
 
-    print('-' * (max_length + 2))
+    nstr = len(msg1) if (len(msg1) > len(msg2)) else len(msg2)
+
+    print('-' * (nstr + 2))
     print(msg1)
     print(msg2)
-    print('-' * (max_length + 2))
-
+    print('-' * (nstr + 2))
